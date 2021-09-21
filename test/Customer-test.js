893158d12f82a1fcdad0bf64b465263b942741b1
Raquel Hill <raquelturing@gmail.com>
2021-09-21 16:44:49 -0600

    Initial commit

---
+++ test/Customer-test.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+import Customer from '../src/Customer';
+import Hotel from '../src/Hotel';
+import sampleCustomerData from '../src/sample-data/sample-customer-data';
+import sampleRoomData from '../src/sample-data/sample-room-data';
+import sampleBookingData from '../src/sample-data/sample-booking-data';
