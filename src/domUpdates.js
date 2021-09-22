df8e76b3a81a1598dc830f7df1f5ab701a579fd7
Raquel Hill <raquelturing@gmail.com>
2021-09-22 16:54:46 -0600

    create login page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,18 @@
-// const logInBtn = document.getElementById('login-button');
-// const reservationBtn = document.getElementById('reservation-button');
-// const yearlyExpenseBtn = document.getElementById('yearly-expense-button');
+// const logInBtn = document.getElementById('sign-in-button');
+const reservationBtn = document.getElementById('reservation-button');
+const yearlyExpenseBtn = document.getElementById('yearly-expense-button');
+const loginBtn = document.getElementById('login-form-submit');
+const loginForm = document.getElementById('login-form');
 
-// const domUpdates = {
-//   logInBtn,
-//   reservationBtn,
-//   yearlyExpenseBtn,
-// };
+const loginErrorMsg = document.getElementById('login-error-msg');
 
-// export default domUpdates;
+const domUpdates = {
+  logInBtn,
+  reservationBtn,
+  yearlyExpenseBtn,
+  loginBtn,
+  loginForm,
+  loginErrorMsg,
+};
+
+export default domUpdates;
